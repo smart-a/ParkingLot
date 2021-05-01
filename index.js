bf0b303fdb3c5678b2a81b1e9564fae5a705e044
smart-a <olalekan.adesoye@gmail.com>
2021-05-01 15:19:55 +0100

    Update in index.js and json file in data folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const carParkRoute = require("./Routers/car-park");
 
 app.use(express.json());
