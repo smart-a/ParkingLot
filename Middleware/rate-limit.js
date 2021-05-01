30ecf60cff03ca32381d0eb1e87b66c42253870e
smart-a <olalekan.adesoye@gmail.com>
2021-05-01 15:12:08 +0100

    Add rate limiter middleware

--- Middleware/rate-limit.js
+++ Middleware/rate-limit.js
@@ -1,1 +1,14 @@
-module.exports.checkIP = (req, res, next) => {};
+const RateLimiter = require("../Helpers/RateLimiter");
+const limiter = new RateLimiter("data/rate.json");
+
+module.exports.checkIPLimit = (req, res, next) => {
+  const reqIP =
+    req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+  limiter.newReqIp = reqIP;
+
+  if (!limiter.checkRateLimit())
+    return res
+      .status(403)
+      .json({ message: "Maximum request reached, please try again" });
+  next();
+};
